000dbddaa0fc9985f1d4795f70793f87be966866
mh孟浩 <hmeng@Ctrip.com>
2014-12-09 17:11:52 +0800

    2014.12.9

--- contorller/user.js
+++ contorller/user.js
@@ -8,9 +8,21 @@ module.exports = function(app) {
 		});
 	});
 
-	app.get('/user/check', function (req, res) {
+	app.get('/user/login, function (req, res) {
 		userm.checkUser({name:req.query.name, password:req.query.password}, function() {
 			res.redirect('/project');
 		});
+	});
+
+	app.get('/user/update', function (req, res) {
+		updateUser: function({sale:req.query.sale, old:req.query.old, telephone:req.query.telephone, born:req.query.born}, function() {
+			res.redirect('/project');
+		}
+	});
+
+	app.get('/user/setPriority', function (req, res) {
+		updateUser: function({priority:req.query.priority}, function() {
+			res.redirect('/project');
+		}
 	});
 };
--- model/userm.js
+++ model/userm.js
@@ -21,19 +21,33 @@ var userm = {
 		connection.query('INSERT INTO user SET ?', {name: p.name, password: p.password, role: p.role}, function(err, result) {
 			  if (err) throw err;
 
-			  cb();
+			  get_cb();
 		});
 	},
 
 
 	createUser: function() {
-		var sql = "CREATE TABLE IF NOT EXISTS `user` (id int auto_increment primary key not null, name varchar(200), password varchar(200), createTime datetime  not null, role int)";
+		var sql = "CREATE TABLE IF NOT EXISTS `user` (id int auto_increment primary key not null, name varchar(200), password varchar(200), createTime datetime  not null, role int, sale int, old int, telephone varchar(20), born datetime, priority int)";
 		connection.query(sql, function(err, rows, fields) {
 			if (err) throw err;
 		});
+	},
+
+	updateUser: function(p, get_cb) {
+		var sql = sprintf("update user set sale = %s, old = %s, telephone = %s, born = %s", p.sale, p.old, p.telephone, p.born);
+		connection.query(sql, function(err, rows, fields) {
+			if (err) throw err;
+			get_cb();
+		});
+	},
+
+	setPriority: function(p, get_cb) {
+		var sql = sprintf("update user set priority = %d", p.priority);
+		connection.query(sql, function(err, rows, fields) {
+			if (err) throw err;
+			get_cb();
+		});
 	}
 };
-
-
 
 module.exports = userm;
